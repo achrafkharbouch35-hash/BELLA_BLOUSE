a13a38b1b968caa4438fde6192a1b274df0508ac
achrafkharbouch35-hash <achrafkharbouch35@gmail.com>
2026-09-10 17:18:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@
    212612345678
 */
 
-const WHATSAPP_NUMBER = "212688403133";
+const WHATSAPP_NUMBER = "212664843900";
 
 
 /* ================= PRODUCTS ================= */
@@ -1110,7 +1110,7 @@ document
 
 
         const url =
-            `https://wa.me/${0688403133}?text=${message}`;
+            `https://wa.me/${0664843900}?text=${message}`;
 
 
         window.open(url, "_blank");
@@ -1123,7 +1123,7 @@ document
 function openWhatsApp(message = "") {
 
     const url =
-        `https://wa.me/${0688403133}?text=${encodeURIComponent(message)}`;
+        `https://wa.me/${0664843900}?text=${encodeURIComponent(message)}`;
 
     window.open(url, "_blank");
 
